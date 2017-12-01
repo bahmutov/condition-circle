3dc2b795ec62015ea35ce2682753d8d607e4bbc4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-01 10:48:27 -0500

    fix: update standard linter

--- src/condition-circle.js
+++ src/condition-circle.js
@@ -6,26 +6,31 @@ const join = require('path').join
 const safeEnv = require('safe-env')
 const SemanticReleaseError = require('@semantic-release/error')
 
-const isToken = (key) {
+const isToken = (key) => {
   return key.toLowerCase().indexOf('token') !== -1
 }
 
-module.exports = async (pluginConfig, { options: { branch } }) => {
+async function conditionCircle (pluginConfig, args) {
+  const options = args.options
+  const branch = options.branch
+
   log('verifying conditions on circle')
   log('need environment variables CIRCLECI and CIRCLE_BRANCH')
   log(safeEnv(isToken, options))
 
   const script = join(__dirname, '../refs.sh')
   spawn.sync(script, [], { stdio: 'inherit' })
-  
+
   if (process.env.CIRCLECI !== 'true') {
-    throw new SemanticReleaseError('Not running on Circle CI');
+    throw new SemanticReleaseError('Not running on Circle CI')
   }
-  
-  const envBranch = process.env.CIRCLE_BRANCH;
+
+  const envBranch = process.env.CIRCLE_BRANCH
   if (branch !== envBranch) {
     throw new SemanticReleaseError(
       `CircleCI using '${envBranch}' not configured publish branch (${branch})`
-    );
+    )
   }
-})
+}
+
+module.exports = conditionCircle
